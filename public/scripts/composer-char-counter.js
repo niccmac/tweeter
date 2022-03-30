cf15ff78f4f42d7bf3e40a2cf863b0b6cbe835b3
Nicole MacLean <nicole.mac0404@gmail.com>
2022-03-29 18:21:48 -0700

    counter turns red as tweet is input

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,9 +4,14 @@ $(document).ready(() => {
   const $tweetTextArea = $('#tweet-text-area');
   const $characterCounterOutput = $("output.counter");
   
-//Counts chatacters in tweet and displays
+  //Counts chatacters in tweet and displays
   $tweetTextArea.on('input', function() {
     $characterCounterOutput.val(140 - $(this).val().length);
+    if ((140 - $(this).val().length) < 0) {
+      $characterCounterOutput.css("color","#bd1125");
+    } else {
+      $characterCounterOutput.css("color","white");
+    }
   });
 
 });
