159a864ebd57944dc72cb12afee80ca8624f3957
Nicole MacLean <nicole.mac0404@gmail.com>
2022-03-29 21:48:56 -0700

    found header.css, moved fixed nav bar essential feature to this folder margin-top. some comments

--- public/scripts/composer-tweetContainer.js
+++ public/scripts/composer-tweetContainer.js
@@ -5,6 +5,8 @@ $(document).ready(() => {
   const $flag = $(".flag-retweet-like-buttons > div > #flag");
   const $retweet = $(".flag-retweet-like-buttons > div > #rt");
   const $like = $(".flag-retweet-like-buttons > div > #heart");
+
+  //Makes specific classes attribute to set variables > makes posted tweet shadow yellow && icons yellow on hover
   $tweetContainer.hover(function() {
     $(this).toggleClass("classWithShadow");
   });
