5e7ae261df2f082cdcaef72a2d04889d35dbcde0
Nicole MacLean <nicole.mac0404@gmail.com>
2022-04-01 08:50:50 -0700

    final pass of chara count, changed error out to show to stop drop down flashing

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,21 +1,18 @@
 $(document).ready(() => {
-  jQuery === $;
-
   const $tweetTextArea = $('#new-tweet-text-area');
   const $characterCounterOutput = $("output.counter");
   
-  //Counts chatacters in tweet and displays
+  //Counts chatacters in tweet and displays error if user types over 140
   $tweetTextArea.on('input', function() {
     $("#tweet-error").hide();
     $characterCounterOutput.val(140 - $(this).val().length);
     if ((140 - $(this).val().length) < 0) {
       $characterCounterOutput.css("color","#bd1125");
       $("#tweet-error-message").text("Your tweet needs to be a little shorter.");
-      $("#tweet-error").slideDown();
+      $("#tweet-error").show();
     } else {
       $characterCounterOutput.css("color","white");
       $("#tweet-error").hide();
     }
   });
-
 });
